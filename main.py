b41b801fc1cce7d734be2e76d4e98409b64b69a4
vedanth6192 <kvedanth15@gmail.com>
2021-03-16 20:43:44 -0700

    Python work

---
+++ main.py
@@ -0,0 +1,33 @@
+#Arthemetic Operators
+num1 = 40
+num2 = 60
+print('num1 + num2 =', num1+num2)
+print('num1 - num2 =', num1-num2)
+print('num1 * num2 =', num1*num2)
+print('num1 / num2 =', num1/num2)
+print(" 5 ^ 4 =", 5**4)
+print('5 % 4 =', 5%4)
+print('5//4 =', 5//4)
+
+num3 = num1 + num2
+print(num3)
+num3 += num2
+print(num3)
+num3 /= num2
+print(num3)
+num3 %= num1
+print(num3)
+
+#Comparison Operators
+print('is num2 > num3 ?', num2>num3)
+print('is num3 = num3 ?', num3==num3)
+print('is num2 != num1 ?', num2!=num1)
+print('is num3 = num2 ?', num3==num2)
+
+#Logical Operators
+x = 5
+print(x > 3 and x < 7 )
+print(x > 5 or x < 3 )
+print(not(x > 7 and x < 3 ))
+
+
